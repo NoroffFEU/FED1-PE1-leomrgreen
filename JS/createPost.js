51c6eb7b07027cda254759c64239a7e7be3fbf63
Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
2024-04-29 16:54:37 +0200

    implemented createPost function and added styling to hero section on home page

---
+++ JS/createPost.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const createForm = document.getElementById('blogPostForm');
+
+  createForm.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const title = document.getElementById('title').value;
+    const body = document.getElementById('body').value;
+    const mediaUrl = document.getElementById('mediaUrl').value;
+    const mediaAlt = document.getElementById('mediaAlt').value;
+
+    createBlogPost(title, body, mediaUrl, mediaAlt);
+  });
+});
+
+function createBlogPost(title, body, mediaUrl, mediaAlt) {
+  const url = 'https://v2.api.noroff.dev/blog/posts/leomrgreen';
+  const accessToken = JSON.parse(sessionStorage.getItem("session"));
+  const postData = {
+    title: title,
+    body: body,
+    media: {
+      url: mediaUrl,
+      alt: mediaAlt
+    }
+  };
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + accessToken 
+    },
+    body: JSON.stringify(postData)
+  })
+  .then(response => {
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    alert('Blog post created successfully!');
+    document.getElementById('blogPostForm').reset(); 
+  })
+  .catch(error => {
+    alert('Error creating blog post: ' + error.message);
+  });
+}
