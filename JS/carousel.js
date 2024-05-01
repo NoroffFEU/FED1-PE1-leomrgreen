e8c80c9d8483872492bde3516cde0f0d7734d022
Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
2024-05-02 00:16:03 +0200

    fixed carousel and implemented darkMode function

---
+++ JS/carousel.js
@@ -0,0 +1,62 @@
+const slidesContainer = document.getElementById('slides');
+let currentIndex = 0;
+const API_URL = 'https://v2.api.noroff.dev/blog/posts/leomrgreen';
+
+function getData() {
+  fetch(API_URL, { method: 'GET' })  
+      .then(response => {
+          if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+      })
+      .then(data => {
+          carouselArray = data.data.sort((a, b) => new Date(b.created) - new Date(a.created)).slice(0, 3);
+          imageCarousel(carouselArray);
+      })
+      .catch(error => {
+          console.error('Error:', error);
+      });
+}
+
+function imageCarousel(carouselArray) {  // this function is from my previous JS1 submission. New thing with this is the new Date and slice method that I use to reach the latest 3 posts from my API array
+    slidesContainer.innerHTML = '';
+    for (let i = 0; i < carouselArray.length; i++) {
+        const carouselImage = document.createElement('div');
+        if (i === 0) {
+            carouselImage.style.display = 'flex';
+        } else {
+            carouselImage.style.display = 'none';
+        }
+        
+        carouselImage.innerHTML = 
+        `<img src="${carouselArray[i].media.url}">
+        <h2>${carouselArray[i].title}</h2>`;
+        
+        carouselImage.setAttribute('class', 'sliderItem');
+        slidesContainer.appendChild(carouselImage);
+    }
+}
+
+function showCurrentImage() {
+  const images = slidesContainer.querySelectorAll('.sliderItem');
+  images.forEach((div, index) => {
+      if (index === currentIndex) {
+        div.style.display = 'flex';
+      } else {
+        div.style.display = 'none';
+      }
+  });
+}
+
+document.getElementById('next').addEventListener('click', function() {
+  currentIndex = (currentIndex + 1) % carouselArray.length;
+  showCurrentImage();
+});
+
+document.getElementById('prev').addEventListener('click', function() {
+  currentIndex = (currentIndex - 1 + carouselArray.length) % carouselArray.length; 
+  showCurrentImage();
+});
+
+getData();
--- JS/navMenu.js
+++ JS/navMenu.js
@@ -10,3 +10,22 @@ burger.addEventListener("click", () => {
     burger.className = "fa-solid fa-bars";
   }
 });
+
+
+const drkModeButton = document.getElementById('drkModeIcon');  
+
+drkModeButton.addEventListener('click', () => {
+  const darkModeElements = document.querySelectorAll('.drkMode');
+  let isDarkMode = document.body.classList.contains('drkModeTrigger'); 
+  
+  // Toggle dark mode class on all elements
+  darkModeElements.forEach(element => {
+    element.classList.toggle('drkModeTrigger');
+  });
+  
+  if (!isDarkMode) {
+    drkModeButton.className = 'fa-regular fa-moon';  
+  } else {
+    drkModeButton.className = 'fa-regular fa-lightbulb'; 
+  }
+});
