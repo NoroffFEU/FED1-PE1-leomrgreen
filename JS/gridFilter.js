dfb74da4090a578d384e3eddf62e61221e79b9ff
Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
2024-05-06 00:07:48 +0200

    minor adjustment

--- JS/gridFilter.js
+++ JS/gridFilter.js
@@ -2,9 +2,6 @@ const gridLayout = document.getElementById('articleGrid');
 const gridBtn = document.getElementById('gridBtn').addEventListener('click', ()=> {
     gridLayout.classList.toggle('active');
   })
-  const columnBtn = document.getElementById('columnBtn').addEventListener('click', ()=> {
-    gridLayout.classList.remove('active');
-  })
 
 
 
